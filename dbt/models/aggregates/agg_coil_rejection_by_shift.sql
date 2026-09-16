d98377f8e12f29492e7b2fce0c6c5bfa8942b96b
takuya kinoshita <kktak02@gmail.com>
2026-09-17 07:21:27 +0900

    update agg

---
+++ dbt/models/aggregates/agg_coil_rejection_by_shift.sql
@@ -0,0 +1,32 @@
+with coil as (
+    select * from {{ ref('fact_coil_rejection') }}
+),
+shift as (
+    select * from {{ ref('dim_shift') }}
+),
+joined as (
+    select
+    {{ dbt_utils.star(
+        ref('fact_coil_completion'),
+        relation_alias='coil',
+        except=['shift_key']
+    ) }},
+    shift.shift_id,
+    shift.group_name,
+    shift.shift_start_timestamp_local,
+    shift.shift_end_timestamp_local
+    from coil
+    left join shift on coil.shift_key = shift.shift_key
+),
+final as (
+    select
+    shift_id,
+    group_name,
+    shift_start_timestamp_local,
+    shift_end_timestamp_local,
+    count(*) as coil_completion_count,
+    sum(coil_weight) as coil_weight_sum
+    from joined
+    group by 1,2,3,4
+)
+select * from final
